48222d686f334684f2e4afdad52e4ac34cb35183
chsgcxy <chsgcxy@outlook.com>
2023-09-06 09:28:29 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-aarch64-none-linux-gnu-gcc -o dhrystone.elf -static dhry_1.c dhry_2.c
+aarch64-none-linux-gnu-gcc -o dhrystone.elf -static -O3 -fno-inline-functions -fno-inline -DGCC -falign-functions=16 -falign-jumps=8 -falign-loops=8 -fomit-frame-pointer -funroll-loops -g dhry_1.c dhry_2.c
+aarch64-none-linux-gnu-objdump --disassemble-all dhrystone.elf > dhrystone.dump
